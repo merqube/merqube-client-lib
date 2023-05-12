2238480f7b32d7ec3069b6587d2a348c319c683b
Tommy Carpenter <tommyjcarpenter@pm.me>
2023-05-12 14:40:41 -0400

    First doc pass + generated types (#20)
    * First doc pass, add type generation
    
    * typo

---
+++ generate_types.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+pip install datamodel-code-generator[http] --upgrade
+curl https://api.merqube.com/api > /tmp/openapi.yaml
+datamodel-codegen --strict-types str bool --enum-field-as-literal one --input-file-type openapi --input /tmp/openapi.yaml --output merqube_client_lib/pydantic_types.py
+black merqube_client_lib/pydantic_types.py
