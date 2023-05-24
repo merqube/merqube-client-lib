0445d6e1329583da2792d3855d775e1d21b26ebb
Tommy Carpenter <tommyjcarpenter@pm.me>
2023-05-24 08:45:07 -0400

    add the failed request id into the `APIError` object, and logging that occurs on failure (#24)

--- merqube_client_lib/exceptions.py
+++ merqube_client_lib/exceptions.py
@@ -8,10 +8,13 @@ from typing import Any
 class APIError(Exception):
     """Generic exception for when our client library hits a non 2xx code from our APIs"""
 
-    def __init__(self, code: int | None = None, response_json: dict[str, Any] | None = None):
+    def __init__(
+        self, code: int | None = None, response_json: dict[str, Any] | None = None, request_id: str | None = None
+    ):
         super().__init__()
         self.code = code
         self.response_json = response_json
+        self.request_id = request_id
 
     def __str__(self) -> str:
-        return f"API Error: code: {self.code}, response_json: {self.response_json}"
+        return f"API Error: code: {self.code}, response_json: {self.response_json}, request_id: {self.request_id}"
